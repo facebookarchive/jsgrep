1f97effa22d5c07c20760a004fec927628162a39
Ryan Patterson <ry@fb.com>
2012-02-22 21:27:13 -0800

    Fix patcher for multiple source modifications
    The old method made the assumption that patches would be applied in source
    order, which shouldn't be a restriction.
    
    Also fixes a bug with replacing property initializers by mangling the AST. Adds
    some test cases to verify that the patcher works.

---
+++ specs/jspatch.spec.js
@@ -0,0 +1,63 @@
+var Matcher = require('../lib/jsgrep.js').Matcher;
+var Narcissus = require('../narcissus');
+var _ = require('underscore');
+var fs = require('fs');
+var path = require('path');
+
+describe('Matcher.getPatchedCode', function() {
+  const fileName = path.resolve(path.join(__dirname, '../tests/jquery.js'));
+  var source = fs.readFileSync(fileName);
+
+  var tests = [
+    {
+      name: 'simple replacement',
+      source: '1',
+      patch: [
+        "-1",
+        "+2"
+      ].join('\n'),
+      result: '2'
+    }, {
+      name: 'two replacements',
+      source: '1, 1',
+      patch: [
+        "-1",
+        "+2"
+      ].join('\n'),
+      result: '2, 2'
+    }, {
+      name: 'remove var',
+      source: 'var a = b;',
+      patch: [
+        "-var a = b;"
+      ].join('\n'),
+      result: ''
+    }, {
+      name: 'match token type',
+      source: '({ "key": value })',
+      patch: [
+        "-key",
+        "+newKey"
+      ].join('\n'),
+      result: '({ "key": value })'
+    }
+  ];
+
+  function runTest(test) {
+    it (test.name, function() {
+      var sourceAst = Narcissus.parser.parse(test.source, 'test source');
+      var matcher = new Matcher(sourceAst);
+      var pattern = Matcher.getPatternFromPatch(test.patch, 'test patch');
+
+      matcher.find(pattern, function(v) {
+        v.node.applyPatch(test.patch, 'test patch');
+      });
+
+      expect(matcher.ast.tokenizer.source).toBe(test.result);
+    });
+  }
+
+  for (var i = 0; i < tests.length; i++) {
+    runTest(tests[i]);
+  }
+});
